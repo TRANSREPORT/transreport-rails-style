f4d903df9c9181a01c84120ddae2c937f90c43ec
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2021-05-27 20:59:49 +0100

    Initialise blank gem in Rubymine

---
+++ lib/transreport/style.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "style/version"
+
+module Transreport
+  module Style
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/transreport/style_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Transreport::Style do
+  it "has a version number" do
+    expect(Transreport::Style::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
